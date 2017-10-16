e7b0a459b25b687c4491b8b9a45f99baba84c125
ozon <drobik.dawid@gmail.com>
2017-10-16 19:44:12 +0200

    first db relations, students&courses

---
+++ db/migrate/20171016172933_create_student_courses.rb
@@ -0,0 +1,8 @@
+class CreateStudentCourses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :student_courses do |t|
+    t.belongs_to :student, index: true
+    t.belongs_to :course, index: true
+    end
+  end
+end
